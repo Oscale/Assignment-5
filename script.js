4e28e742700aeffedd92a2db6822b6f5d7dde9db
Oscar A.G <>
2021-05-07 18:53:30 +0000

    Some spacing for the linter to be happy

--- script.js
+++ script.js
@@ -2,14 +2,14 @@ document.getElementById('cal').addEventListener('click', Calculate)
 
 let radiusn = 0
 let diametern = 0
-let pien = 0 
+let pien = 0
 let rad2 = 0
 let areao = 0
 let ciro = 0
 
 function Calculate () {
   diametern = document.getElementById('diameterin').value
-   
+
   diametern = parseFloat(diametern)
   radiusn = parseFloat(radiusn)
   pien = parseFloat(pien)
@@ -17,21 +17,20 @@ function Calculate () {
   areao = parseFloat(areao)
   ciro = parseFloat(ciro)
 
-  radiusn = ( diametern / 2 )
+  radiusn = (diametern / 2)
 
   pien = (3.14)
-  
-  /* The code below is the calculation for the 
+
+  /* The code below is the calculation for the
   area of the circle */
 
   rad2 = (radiusn * radiusn)
 
-  areao = (pien*rad2)
- 
+  areao = (pien * rad2)
+
   document.getElementById('anwserbox1').innerHTML = areao
 
-
-  /* The code below is the calculation of the  circimfrance of a circle.*/
+  /* The code below is the calculation of the  circimfrance of a circle. */
 
   ciro = (pien * radiusn * 2)
 
